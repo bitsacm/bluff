0a0526ac2ede19e9ad34f6c55719f378ba743207
Pratham Gupta <f20190051@pilani.bits-pilani.ac.in>
2020-06-30 13:34:31 +0530

    try catch for database connect

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useFindAndModify: false,
-	useUnifiedTopology: true 
-}).then(async (db) => {
-	console.log('Successfully connected to database :)')
-}).catch((error) => {
-	console.log('Not able to connect to database :(')
-})
+try {
+  mongoose.connect(process.env.MONGODB_URL, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true 
+	}).then(async (db) => {
+		console.log('Successfully connected to database :)')
+	}).catch((error) => {
+		console.log('Not able to connect to database :(')
+	})
+} catch (e) {
+	console.log(e)
+}

8f531b9e3391ab2b175ed9a84132cc6fd94ec141
Pratham Gupta <54350608+pratham1002@users.noreply.github.com>
2020-06-30 13:25:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-	console.log('Server is up on port ' + port);
+  console.log('Server is up on port ' + port);
 });

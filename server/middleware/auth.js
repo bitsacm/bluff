ff325109046ad5e3af79f83a37f43d03ab94f3d1
Pratham Gupta <54350608+pratham1002@users.noreply.github.com>
2020-08-04 21:54:04 +0530

    Redirect URL corrected

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -25,7 +25,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "https://sarvasvakhare-bluff.herokuapp.com/auth/google/home",
+  callbackURL: "https://acm-bluff.herokuapp.com/auth/google/home",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 // function for verify and storing user into database

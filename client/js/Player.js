d59ab3026fa21cb6066fd737bfc674b071e3c620
Dhairya Shah <dhairyarshah15@gmail.com>
2020-08-05 15:29:58 +0530

    Rendering each players cards on the screen

---
+++ client/js/Player.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+// Creating the player containing the name of the player,number of cards of the player and cards that the player has
+
+class Player {
+  constructor (name) {
+    this.playerName = name
+    this.playerCards = []
+    this.numberOfCards = 0
+  }
+}

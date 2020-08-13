532089147e9fa71d8919c2c3f0b16180818e780c
SARVASVA KHARE <64934816+sarvasvaKhare@users.noreply.github.com>
2020-08-13 10:22:23 +0530

    server side code

--- server/index.js
+++ server/index.js
@@ -1,8 +1,86 @@
 //importing app
 const app = require('./app')
+const http = require('http')
+const socket = require('socket.io')
 //port as enviroment variable
 const port = process.env.PORT || 3000
+const server = http.Server(app)
 //server up check
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('Server is up on port ' + port)
 })
+
+//socket setup
+const io = socket(server)
+
+// on connecting with  client socket
+io.on('connection', (socket) => {
+  // join event listner
+  socket.once('join', function (roomname) {
+    // checks for maximum users and if game started?
+    if ((io.nsps['/'].adapter.rooms[roomname] && io.nsps['/'].adapter.rooms[roomname].length > 11) || (io.nsps['/'].adapter.rooms[roomname] && io.nsps['/'].adapter.rooms[roomname].allowed != undefined)) {
+    } else {
+      socket.join(roomname)      // if passed checks joins room
+      socket.broadcast.emit('broadcast', 'new player joined');      // brodcast every other a player has joined
+      socket.emit('connectToRoom', "You are in room " + roomname)   // emit user the roomname
+    }
+  })
+
+  // set username event listner
+  socket.on('setUsername', function (data) {
+    var usernameTaken = 0;                                   // var for checking unique username
+    io.in(data.roomname).clients((error, clients) => {      // getting all clients in room
+      if (error) throw error;
+      // loop for checking username of every other player with given username
+      for (var i = 0; i < clients.length; i++) {
+        var user = io.sockets.sockets[clients[i]];    // saving socket into user 
+
+        if (user.username == data.username) {         // if equal therfore username taken
+          usernameTaken = 1;                          // setting var to 1
+          break;                                      // break from loop
+        }
+      }
+
+      if (!usernameTaken) {
+        socket.username = data.username           // setting username in socket of that client
+        socket.emit('userSet', { username: data.username })   // emiting username to client side
+      } else {
+        // emitting dupilicate username to client side
+        socket.emit('userExists', data.username + ' username is taken! Try some other username.');
+        socket.emit('newPrompt');   //emitting new Prompt for new username
+      }
+    })
+  });
+
+  // gameStart listner
+  socket.on('gameStart', (data) => {
+    var usernames = [];       // array for storing usernames
+    io.in(data.roomname).clients((error, clients) => {    // getting all clients in a room
+      if (error) throw error;
+      clients.forEach(client => {
+        var person = io.sockets.sockets[client]
+        usernames.push(person.username)         // adding their username to usernames array
+      });
+    })
+    if (io.nsps['/'].adapter.rooms[data.roomname].length > 1) {
+      io.nsps['/'].adapter.rooms[data.roomname].allowed = 1;  // setting var for game started true
+      io.in(data.roomname).emit('usersList', usernames)       // emitting usernames list to clients
+    } else {
+      io.in(data.roomname).emit('lessPlayers', 'need more to start game')
+    }
+
+  })
+  // cardHandToDeck event listner
+  socket.on('cardHandToDeck', (ids) => {
+    io.in(roomname).emit('cardsInDeck', ids);
+  })
+  // endGame event listner
+  socket.on('endGame',(data)=>{
+    io.in(data.roomname).emit('redirect','/')
+  })
+  // dissconection event
+  socket.once('disconnect', function () {
+    console.log('disconnected')
+  })
+})
+
--- server/routers/index.js
+++ server/routers/index.js
@@ -1,41 +1,49 @@
-//router setup
+// router setup
 const express = require('express')
 const router = new express.Router()
+// var bodyParser = require('body-parser')
+// const Room = require('../models/rooms')
+// var urlencodedParser = bodyParser.urlencoded({ extended: false })
+// authentication setup
+const passport = require('passport')
 
-//authentication setup
-const passport = require("passport");
-
-//index route
+// index route
 router.get('', (req, res) => {
-  res.render('index')
+  res.redirect('/rooms')
 })
 
-//google auth entry route
-router.get('/login',(req,res)=>{
-res.redirect('/auth/google')
+// google auth entry route
+router.get('/login', (req, res) => {
+  res.redirect('/auth/google')
 })
 
-//route authenticating user by google
+// route authenticating user by google
 router.get('/auth/google',
-  passport.authenticate('google',{scope: ["profile"]})
+  passport.authenticate('google', {
+    scope: ['profile']
+  })
 )
 
-//route redirected to after sucessfull authentication from google
-router.get('/auth/google/home',passport.authenticate('google', { failureRedirect: "/login" }),
-function(req, res) {
-  res.redirect("/result");
-});
-
-// redirected route to render result page
-router.get('/result',(req,res)=>{
-  if (req.isAuthenticated()){
-    res.render('index')
-    }else{
-      res.redirect('/login')
-    }
-  
-  
+// route redirected to after sucessfull authentication from google
+router.get('/auth/google/home', passport.authenticate('google', {
+  failureRedirect: '/login'
+}),
+function (req, res) {
+  res.redirect('/result')
 })
 
-//exporting module for app.js
-module.exports = router
+// redirected route to render result page
+router.get('/result', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render('index')
+  } else {
+    res.redirect('/login')
+  }
+})
+
+// route for socket testing
+router.get('/rooms', (req, res) => {
+  res.render('test')
+})
+// exporting module for app.js
+module.exports = { router }

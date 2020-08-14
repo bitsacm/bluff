07fc98bdfc46423620f88a4551d4756c9443fd68
SARVASVA KHARE <64934816+sarvasvaKhare@users.noreply.github.com>
2020-08-14 21:52:02 +0530

    Delete rooms.js

--- server/models/rooms.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const ttl = require('mongoose-ttl');
-
-const roomSchema = new mongoose.Schema({
-    Roomname :{ type:String, unique:true},
-});
-roomSchema.plugin(ttl, { ttl: '2m' });
-const Room = mongoose.model('room', roomSchema,);
-module.exports = Room

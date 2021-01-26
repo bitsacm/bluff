5efed58617bdd91596aeba71fa03387415baef5c
madhavgupta211 <56752137+madhavgupta211@users.noreply.github.com>
2021-01-27 01:01:50 +0530

    react configuration completed

--- frontend/src/Store/Reducers/game.js
+++ frontend/src/Store/Reducers/game.js
@@ -3,7 +3,9 @@ import Game from '../../Game';
 
 const initialState = {
   gameFetched: false,
-  gameData: new Game()
+  gameData: new Game(),
+  hasEnded: false,
+  winner: ''
 };
 
 const gameReducer = (state = initialState, action) => {
@@ -13,7 +15,9 @@ const gameReducer = (state = initialState, action) => {
       newed.state = action.payload.state;
       newed.cards = action.payload.cards;
       console.log(newed);
-      return { gameFetched: true, gameData: newed};
+      return { ...state, gameFetched: true, gameData: newed};
+    case actionTypes.GAME_END: 
+      return { ...state, hasEnded: true, winner: action.payload};
     default:
       return {
         ...state

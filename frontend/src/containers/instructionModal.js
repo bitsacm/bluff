1c188ecbf1a8535c99e2a1f13819d04f865c870a
Pratham Gupta <54350608+pratham1002@users.noreply.github.com>
2021-03-13 22:58:35 +0530

    Add Instructions (#32)
    * Update instructionModal.js
    
    * Update Game.js

--- frontend/src/containers/instructionModal.js
+++ frontend/src/containers/instructionModal.js
@@ -8,7 +8,15 @@ class InstructionModal extends Component {
       <Modal open = {this.props.modalVisible} toggle = {this.props.toggleModal}>
         <ModalHeader>Instructions</ModalHeader>
         <ModalBody>
-          
+          The goal is simple, get rid of all your cards, by hook or crook. 
+          <br/>
+          Careful there, for you might get caught.
+          <br/>
+          Each round begins with someone picking a rank, say Ace or 10 or whatever pleases them. Now you shall add cards of the same rank (or claim to have done so) or you could just pass.
+          <br/>
+          If you feel the last person might is bluffing, you shall call thier bluff and the liar gets all the cards on the table (or you do for the lack of judgement 🙂).
+          <br/>
+          The first one to lose all their cards shall be the Bluffmaster.
         </ModalBody>
       </Modal>
     );
